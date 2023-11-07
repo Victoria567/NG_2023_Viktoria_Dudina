65fb8fdb710077e7baf37603b4590e775c4fe796
Victoria567 <147425913+Victoria567@users.noreply.github.com>
2023-11-07 22:58:26 +0200

    Update main.py

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -2,37 +2,37 @@ import math
 
 operation = input ("Enter + for addition, - for subtraction, * for multiplication, / for division, ^ for exponentiation, k for root extraction: ")
 
-num1 = float(input("Number: 1"))
-num2 = float(input("Number: 2"))
+num1 = float(input("Number 1: "))
+num2 = float(input("Number 2: "))
 
-if operation == "+":
-    result = num1 + num2
+match operation:
+  case "+":
+    result = num1 + num2 
     print(f"The sum of numbers: {result}")
-
-elif operation == "-":
-    result = num1 - num2
+  
+  case "-":
+    result = num1 - num2 
     print(f"The difference of numbers: {result}")
-
-elif operation == "*":
-    result = num1 * num2
+  
+  case "*":
+    result = num1 * num2 
     print(f"The product of numbers: {result}")
-
-elif operation == "/":
+  
+  case "/":
     if num2 != 0:
-        result = num1 / num2
-        print(f"The quotient of numbers: {result}")
-    else:
-        print("Error. Division by 0 is forbidden.")
-
-elif operation == "^":
+      result = num1 / num2 
+      print(f"The quotient of numbers: {result}")
+    else: 
+      print("Error. Division by 0 is forbidden.")
+  
+  case "^": 
     result = math.pow(num1, num2)
     print(f"{num1} to the power {num2}: {result}")
-
-elif operation == "k":
+  
+  case "k":
     if num1 >= 0:
-        result = math.pow(num1, 1/num2)
-        print("The root of a number {num2} to the power {num1}: {resultat}")
-
-
-else:
+      result = math.pow(num1, 1/num2)
+      print(f"The root of a number {num2} to the power {num1}: {result}")
+ 
+  case _:
     print("Wrong choice. Choose: +, -, *, /, ^, k")
