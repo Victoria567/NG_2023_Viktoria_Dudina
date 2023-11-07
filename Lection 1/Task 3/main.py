b8409bc37e7aadb1adf6beabfe6d462cece01e44
Victoria567 <147425913+Victoria567@users.noreply.github.com>
2023-11-07 23:17:56 +0200

    Update main.py

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,14 +1,15 @@
-choice = input("1 - Celsius to Fahrenheit. 2 - if from Fahrenheit to Celsius")
+choice = input("1 - Celsius to Fahrenheit. 2 - if from Fahrenheit to Celsius\n")
 
-if choice == "1":
-    celsius = float(input("Temperature in Celsius"))
+match choice:
+  case "1":
+    celsius = float(input("Temperature in Celsius\n"))
     fahrenheit = celsius * 9/5 + 32
     print(f"Temperature in degrees Fahrenheit: {fahrenheit: .2f}")
-
-elif choice == "2":
-    fahrenheit = float(input("Temperature in Fahrenheit"))
+  
+  case "2":
+    fahrenheit = float(input("Temperature in Fahrenheit\n"))
     celsius = (fahrenheit - 32) * 9/5
     print(f"Temperature in degrees Celsius: {celsius: 2f}")
-
-else:
+  
+  case _:
     print ("Error. Enter 1 or 2")
