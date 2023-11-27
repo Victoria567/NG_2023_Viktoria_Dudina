78bea1bd5b227a4919368ee82eed43086c30a8a4
Victoria567 <147425913+Victoria567@users.noreply.github.com>
2023-11-27 11:42:03 +0200

    Update main.py

--- Lection 1/Task 2/main.py
+++ Lection 1/Task 2/main.py
@@ -1,3 +1,6 @@
-print ("Enter 2 numbers separated by a space")
-numbers = input()
-print ("You entered:", numbers)
+numbers = input("Enter 2 numbers separated by a space: ")
+num1 = int(numbers.split()[0])
+num2 = int(numbers.split()[1])
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is {sum}")
